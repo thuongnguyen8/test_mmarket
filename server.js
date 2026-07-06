e74790ca61965914cfc0b4f7a2527334bc67e666
LocChau1999 <thuong.nguyen8@mservice.com.vn>
2026-07-06 22:00:33 +0700

    Add Express server and Railway config for deployment

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Handle 404 - return index.html for SPA
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`🛒 Chợ MoMoer running on port ${PORT}`);
+});
